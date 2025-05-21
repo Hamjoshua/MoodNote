e579a1ecf0a61697c8f5017ce938bf7999455df5
Hamjoshua <biryukov-10@list.ru>
2025-05-21 10:36:04 +0700

    Рабочая навигация между фрагментами

--- app/src/main/java/com/example/moodnote/MainActivity.kt
+++ app/src/main/java/com/example/moodnote/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.moodnote
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.moodnote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        var appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.mainFragment,
+                R.id.noteFormFragment,
+                R.id.statisticsFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.navigation.setupWithNavController(navController)
     }
 }
