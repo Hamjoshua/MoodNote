0bfe3e54677d8b4fc1a728222f46e0d1b197b526
Hamjoshua <biryukov-10@list.ru>
2025-05-22 10:23:15 +0700

    Добавлен диалоги времени на фильтр

--- app/src/main/java/com/example/moodnote/ui/FilterFragment.kt
+++ app/src/main/java/com/example/moodnote/ui/FilterFragment.kt
@@ -1,60 +1,90 @@
 package com.example.moodnote.ui
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.text.format.DateFormat
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TimePicker
 import com.example.moodnote.R
+import com.example.moodnote.databinding.FragmentFilterBinding
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FilterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FilterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+class FilterFragment : Fragment(), TimePickerDialog.OnTimeSetListener {
+    private lateinit var binding: FragmentFilterBinding
+    private lateinit var currentButton: Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentFilterBinding.inflate(layoutInflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_filter, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FilterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FilterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initButtons()
+    }
+
+    private fun initButtons() {
+        // TODO
+        binding.dateToButton.setOnClickListener {
+            var minDate: Long? = null
+            binding.dateFromButton.text?.let {
+                val date = SimpleDateFormat("yyyy/MM/dd").parse(it.toString())
+                minDate = date.time
             }
+            currentButton = binding.dateToButton
+            initDateTimeDialogs(minDate)
+        }
+
+        binding.dateFromButton.setOnClickListener {
+            currentButton = binding.dateFromButton
+            initDateTimeDialogs()
+        }
+    }
+
+    private fun initDateTimeDialogs(minDate: Long? = null) {
+        var date: String = ""
+
+        val dateDialog: DatePickerDialog = DatePickerDialog(requireContext())
+        minDate?.let {
+            dateDialog.datePicker.minDate = minDate
+        }
+
+        dateDialog.show()
+
+        dateDialog.setOnDateSetListener { datePicker, y, m, d ->
+            Log.d("datepicker", "Date set")
+            date = "$y/$m/$d"
+            currentButton.text = date
+
+            val c = Calendar.getInstance()
+            val hour = c.get(Calendar.HOUR_OF_DAY)
+            val minute = c.get(Calendar.MINUTE)
+
+            val timePickerDialog: TimePickerDialog =
+                TimePickerDialog(
+                    requireContext(), this, hour, minute,
+                    DateFormat.is24HourFormat(activity)
+                )
+
+            timePickerDialog.show()
+        }
+    }
+
+    override fun onTimeSet(p0: TimePicker?, h: Int, m: Int) {
+        val time = "$h:$m"
+        currentButton.text = "${currentButton.text} $time"
     }
 }
